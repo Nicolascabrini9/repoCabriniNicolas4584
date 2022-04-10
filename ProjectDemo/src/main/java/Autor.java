029c1ee1abbb0b84a8ea784f950b1f82db08ba33
ncabr <ncabr@DESKTOP-IUP1DIT>
2022-04-10 02:47:36 -0300

    Atributos y Constructores de clase Libro y Autor

--- ProjectDemo/src/main/java/Autor.java
+++ ProjectDemo/src/main/java/Autor.java
@@ -1,4 +1,12 @@
 
 public class Autor {
+	private String nombre;
+	public Autor ( ) {
+		
+	}
+	public Autor(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
 
 }
--- ProjectDemo/src/main/java/ar/edu/unju/fi/model/Libro.java
+++ ProjectDemo/src/main/java/ar/edu/unju/fi/model/Libro.java
@@ -1,5 +1,17 @@
 package ar.edu.unju.fi.model;
 
 public class Libro {
+	private String Nombre;
+	private int codigo;
+	private String autor;
+	public Libro () {
+		
+	}
+	public Libro(String nombre, int codigo, String autor) {
+		super();
+		Nombre = nombre;
+		this.codigo = codigo;
+		this.autor = autor;
+	}
 
 }
